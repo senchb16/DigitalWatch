f483cdb2880806e5dea3fc96cc20df17001118c4
Chris Schennach <senchb16@new.edvhtl.at>
2019-03-09 20:29:38 +0100

    Klassen erstellt

---
+++ Schennach_DigitalWatch/src/digitalwatch/Digit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package digitalwatch;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Christoph
+ */
+public class Digit extends JLabel {
+    private int value;
+
+    public void setValue(int value) {
+        this.value = value;
+        this.setIcon(new ImageIcon("src/icons/"+value+".png"));
+    }
+    public Digit(int number) {
+        this.setSize(40,67);
+        this.setIcon(new ImageIcon("src/icons/"+number+".png"));
+        value = number;
+    }
+
+   
+    
+    
+    
+}
